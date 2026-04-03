faf9c12010e153150e9226a20d78561cd0256929
ellenallivv <markovkacorp@gmail.com>
2026-04-03 17:40:22 +0500

    fix paths

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,26 @@ import {
   TextDivider,
   TimingComponent,
 } from "./components";
+import invitedImg from "./assets/invited.png";
+import locationImg from "./assets/location.png";
+import timingImg from "./assets/timing.png";
+import dressImg from "./assets/dress.png";
+import questionsImg from "./assets/questions.png";
 
 function App() {
   return (
     <AppLayout>
       <LineDivider />
       <Header />
-      <TextDivider url="src/assets/invited.png" size="10em" />
+      <TextDivider url={invitedImg} size="10em" />
       <MainContent />
-      <TextDivider url="src/assets/location.png" size="5.5em" />
+      <TextDivider url={locationImg} size="5.5em" />
       <LocationComponent />
-      <TextDivider url="src/assets/timing.png" size="6.7em" />
+      <TextDivider url={timingImg} size="6.7em" />
       <TimingComponent />
-      <TextDivider url="src/assets/dress.png" size="6.7em" />
+      <TextDivider url={dressImg} size="6.7em" />
       <Dress />
-      <TextDivider url="src/assets/questions.png" size="5.5em" />
+      <TextDivider url={questionsImg} size="5.5em" />
       <Questions />
       <LineDivider />
     </AppLayout>
